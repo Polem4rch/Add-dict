62905fe36a1e865f7f5fe14a6c9587cb092b677d
NullWin <44825899+NullWin@users.noreply.github.com>
2020-08-01 01:35:18 -0300

    Update DictAdd.py

--- DictAdd.py
+++ DictAdd.py
@@ -1,11 +1,6 @@
-# meter txt con palabras, le agregue las palabras extra que queres que tenga y saque un txt nuevo con las palabras del txt original + las nuevas
-
-
-
-
 import argparse
 
-parser = argparse.ArgumentParser(description='add a text file, add some fuzz ')
+parser = argparse.ArgumentParser(description='usage: python3 dictadd.py -f txtfile -w new word to append ')
 parser.add_argument('-f')
 parser.add_argument('-w')
 args = parser.parse_args()
