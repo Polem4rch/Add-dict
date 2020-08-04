67032422ea0ac35021cf6845ab213b5a8a10ca98
zzyyxxww <guidotarsia@gmail.com>
2020-08-04 19:47:07 -0300

    Add -w and -p arguments

--- adddict.py
+++ adddict.py
@@ -1,4 +1,6 @@
+#!/bin/env python3
 import argparse
+import os
 
 print("               ")
 
@@ -20,11 +22,17 @@ print("        [FILE CREATED]")
 parser = argparse.ArgumentParser(description='usage: python3 dictadd.py -f txtfile -w new word to append ')
 parser.add_argument('-f')
 parser.add_argument('-w')
+parser.add_argument('-p', '--prefix')
 args = parser.parse_args()
-  
 
-with open(args.f) as f:
-    lines = f.readlines()
-new_lines = [''.join([x.strip(), (args.w), '\n']) for x in lines]
-with open('new.txt', 'w') as f:
-    f.writelines(new_lines)
+words = args.w.split(',')
+output_name = 'catput.txt'
+
+# delete output before starting
+# os.remove(output_name)
+with open(args.f) as input, open(output_name, 'w') as output:
+    for line in input:
+        accum = ''
+        for w in words:
+            accum += w
+            output.write(args.prefix + line.rstrip() + accum + os.linesep)
